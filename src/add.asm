62d242f8ce134a924724fc62e53086c11069300f
drdanick <di.da.nick@gmail.com>
2017-02-28 00:05:20 -0800

    Make use of new machine/assembly features

--- src/add.asm
+++ src/add.asm
@@ -1,21 +1,17 @@
 ; Simple ADD demonstration
 ;
 ; This program will push the values 0xA0 and 0x0A into
-; the stack, then pop both values into the second accumulator 
+; the stack, then pop both values into the second accumulator
 ; with the ADD operation. The second accumulator will then hold 0xAA.
 ;
-ASET 0    ; select and zero $a0
-AND 0x00
+ASET 0
 
-ADD 0x0A  ; Push 0xA0 to the stack
-SHFl 4    ; We want 0xA0, so shift left 4 times
-SPUSH
+LAR 0xA0  ; Load value of 0xA0 into $a0...
+SPUSH     ; ...and push it into the stack
 
-AND 0x00  ; Zero $a
+LAR 0x0A  ; Load value of 0x0A into $a0...
+SPUSH     ; ...and push it into the stack
 
-ADD 0x0A  ; Push 0x0A to the stack
-SPUSH
-
-ASET 1    ; Do the following to $a1
-SPOP      ; Pop 0x0A from the stack, then pop/add 0xA0
-SPOP ADD
+ASET 1    ; Pop 0x0A from the stack into $a1...
+SPOP
+SPOP ADD  ; ...then pop 0xA0 from the stack and add it to $a1
